1a6e4e3a3837723ee0f3005b4b29d64d2bae5807
Laszlo Skumat <laszlo.skumat@ericsson.com>
2017-05-30 09:10:33 +0200

    First draft of parser

---
+++ src/mathphraseparser/MathPhraseParser.java
@@ -0,0 +1,20 @@
+package mathphraseparser;
+
+public final class MathPhraseParser {
+
+	private String result;
+	
+	public MathPhraseParser(String input) {
+		try {
+			// TODO: fill up result
+			result = "42";
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+	public String getResult() {
+		return result;
+	}
+
+}
