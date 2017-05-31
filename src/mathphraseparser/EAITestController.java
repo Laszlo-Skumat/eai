9d84c5bbf475f63a6e71f429ca4297adeae199b1
Laszlo Skumat <laszlo.skumat@ericsson.com>
2017-05-31 11:58:14 +0200

    Some error handling added
    Added an error handling staff to display errors raised in the Spring
    framework that can't be caught in this Java code

--- src/mathphraseparser/EAITestController.java
+++ src/mathphraseparser/EAITestController.java
@@ -1,20 +1,106 @@
 package mathphraseparser;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.autoconfigure.web.ErrorAttributes;
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Controller
 @EnableAutoConfiguration
-public class EAITestController {
+public class EAITestController implements ErrorController{
+
+    private ErrorAttributes errorAttributes;
+    private final static String ERROR_PATH = "/error";
+
+    private boolean getTraceParameter(HttpServletRequest request) {
+        String parameter = request.getParameter("trace");
+        if (parameter == null) {
+            return false;
+        }
+        return !"false".equals(parameter.toLowerCase());
+    }
+
+    /**
+     * Part of error handling, used from a tutorial
+     */
+    private Map<String, Object> getErrorAttributes(HttpServletRequest request,
+    		boolean includeStackTrace) {
+    	RequestAttributes requestAttributes = new ServletRequestAttributes(request);
+    	return this.errorAttributes.getErrorAttributes(requestAttributes,
+    			includeStackTrace);
+    }
+
+    /**
+     * Part of error handling, used from a tutorial
+     */
+    private HttpStatus getStatus(HttpServletRequest request) {
+    	Integer statusCode = (Integer) request
+    			.getAttribute("javax.servlet.error.status_code");
+    	if (statusCode != null) {
+    		try {
+    			return HttpStatus.valueOf(statusCode);
+    		}
+    		catch (Exception ex) {
+    		}
+    	}
+    	return HttpStatus.INTERNAL_SERVER_ERROR;
+    }
+
+    /**
+     * Entry point
+     * @param phrase
+     * @return The result of the evaluation of the math phrase
+     */
     @RequestMapping(value="/person/get/{id}", method=RequestMethod.GET, produces="application/json")
     @ResponseBody
     String home(@PathVariable("id") String id) {
-        return "{\"result\": \"Ez az:"+id+"\"}";
+    	return "{\"result\": \"Ez az:"+id+"\"}";
     }
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(EAITestController.class, args);
+    /**
+     * Controller for the test web service
+     * @param errorAttributes
+     */
+    public EAITestController(ErrorAttributes errorAttributes) {
+    	this.errorAttributes = errorAttributes;
     }
+    
+	@Override
+	public String getErrorPath() {
+		return ERROR_PATH;
+	}
+
+	/**
+	 * Display error page  
+	 * @param request
+	 * @return Error page
+	 */
+    @RequestMapping(value = ERROR_PATH)
+    @ResponseBody
+    public ResponseEntity<Map<String, Object>> error(HttpServletRequest request) {
+        Map<String, Object> body = getErrorAttributes(request, getTraceParameter(request));
+        HttpStatus status = getStatus(request);
+        return new ResponseEntity<Map<String, Object>>(body, status);
+    }
+    
+    /**
+     * Application entry point
+     * TODO: move out from this class 
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+    	SpringApplication.run(EAITestController.class, args);
+    }
+
 }
